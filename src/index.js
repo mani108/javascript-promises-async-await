202a7e83e2aa83abe6b8e0b213feed30a11cc1ae
Chintamani.Puntambekar <Chintamani.Puntambekar@cytel.com>
2020-12-04 13:18:53 +0530

    Comit 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import fetchWithTimeout from "services.js";
+
+const movies = require("data/movies.json");
 export function fetchMovies(){
-    
+    const resolvedFunction = () => movies;
+    fetchWithTimeout(1000)
+        .then(resolvedFunction());
 }
+
+const moviePromise = fetchMovies();
